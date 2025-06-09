1a98ee8642bb6e6fa5a53ff778e04efa1eeeea3b
Ask Iver <sylteaskiver@yahoo.com>
2025-06-09 17:14:43 +0200

    Add anonymization file

---
+++ anonymize_turbine_number.py
@@ -0,0 +1,13 @@
+
+turbine_dict = {}
+
+for idx, value in enumerate(["07", "21", "41"]):
+    turbine_dict[chr(65 + idx)] = value
+    turbine_dict[value] = chr(65 + idx)
+
+def replace_turbine_number(value):
+    # If value is a single character or single number, return single term back
+    if len(value) < 3:
+        return turbine_dict.get(value)
+    # If not, change the path to a correct term
+    return value.replace("07", "A").replace("21", "B").replace("41", "C")
